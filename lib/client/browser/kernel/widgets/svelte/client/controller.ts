a9a75ef4f5dd8232110d4806cf2810127aced4df
Felix Tovar <ftovar@jidadesarrollos.com>
2022-03-08 15:10:29 -0400

    Compilacion | update v0.99.18 - builder actions.specs

--- lib/client/browser/kernel/widgets/svelte/client/controller.ts
+++ lib/client/browser/kernel/widgets/svelte/client/controller.ts
@@ -1,53 +1,19 @@
-import {beyond, BeyondWidgetController} from "@beyond-js/kernel/core/ts";
+import {BeyondWidgetController} from "@beyond-js/kernel/core/ts";
 
 export /*bundle*/
 abstract class SvelteWidgetController extends BeyondWidgetController {
-    #styles;
-    #hmrStylesChanged = styles => {
-        const {shadowRoot} = this.component;
-        const previous: Node = shadowRoot.querySelectorAll(`:scope > [bundle="${styles.id}"]`)[0];
-
-        previous && shadowRoot.removeChild(previous);
-        styles.css && shadowRoot.appendChild(styles.css);
-    };
-
-    #body: HTMLSpanElement;
-
-    render() {
-        this.#body?.remove();
-
-        this.#body = document.createElement('span');
-        this.component.shadowRoot.appendChild(this.#body);
-
-        const {Widget} = this.bundle.package().exports.values;
-        if (!Widget) {
-            const message = `Widget "${this.element}" does not export a Widget class`;
-            console.error(message);
-            return;
-        }
-
-        new Widget({target: this.#body});
+    _mount(props: any) {
+        const hydrate = this.hydratable;
+        new this.Widget({target: this.body, hydrate, props});
     }
 
     mount() {
-        this.render();
-
-        this.bundle.dependencies.forEach(resource => {
-            if (!beyond.bundles.has(resource)) return;
-
-            const dependency = beyond.bundles.get(resource);
-            const {styles} = dependency;
-            if (styles.appended || !styles.css) return;
-
-            const css: any = styles.css;
-            this.component.shadowRoot.appendChild(css);
-            styles.on('change', this.#hmrStylesChanged);
+        this._mount({
+            component: this.component,
+            store: this.store
         });
+    }
 
-        // Append styles and setup styles HMR
-        this.#styles = this.bundle.styles;
-        const {css} = this.#styles;
-        css && this.component.shadowRoot.appendChild(css);
-        this.#styles.on('change', this.#hmrStylesChanged);
+    unmount() {
     }
 }
