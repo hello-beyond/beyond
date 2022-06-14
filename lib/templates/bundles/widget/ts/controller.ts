a0da7d0910b832c9a2fb5b4841083d8b4534b31d
Felix Tovar <ftovar@jidadesarrollos.com>
2022-06-14 13:06:58 -0400

    Compilacion | beyond compilation v0.99.27

--- lib/templates/bundles/widget/ts/controller.ts
+++
@@ -1,9 +0,0 @@
-import {PageReactWidgetController} from '@beyond-js/kernel/react-widget/ts';
-import {Widget} from "./views/code";
-
-export /*bundle*/
-class Controller extends PageReactWidgetController {
-    get Widget() {
-        return Widget;
-    }
-}
--- lib/templates/bundles/widget/vue/controller.ts
+++
@@ -1,9 +0,0 @@
-import {VueWidgetController} from '@beyond-js/kernel/vue-widget/ts';
-import Widget from "./widget.vue";
-
-export /*bundle*/
-class Controller extends VueWidgetController {
-    get Widget() {
-        return Widget;
-    }
-}
